e152332c35ee4dcb4474ab43962416b0bc695d3d
Graham Borland <graham.borland@gramboid.com>
2019-12-04 10:14:28 +0100

    Update sourceSets and targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.3.60"
+    kotlin("multiplatform") version "1.3.61"
 }
 
 group = "com.gramboid"
@@ -10,9 +10,17 @@ repositories {
 }
 
 kotlin {
-    /* Targets configuration omitted. 
-    *  To find out how to configure the targets, please follow the link:
-    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+    jvm()
+    js { browser() }
+    iosX64()
+
+    val os = org.gradle.internal.os.OperatingSystem.current()
+    val hostTarget = when {
+        os.isLinux -> linuxX64("linux")
+        os.isMacOsX -> macosX64("macos")
+        os.isWindows -> mingwX64("mingw")
+        else -> throw IllegalStateException("Your OS [$os] is not supported!")
+    }
 
     sourceSets {
         val commonMain by getting {
@@ -24,6 +32,29 @@ kotlin {
             dependencies {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(kotlin("test-junit"))
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-js"))
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
             }
         }
     }
